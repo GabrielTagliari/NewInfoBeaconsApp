d80ceb5bc2296e2f08bfcfd1035355888662f0c7
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2016-10-01 15:57:04 -0300

    Adicionado ranging beacons

--- app/src/main/java/com/infobeacons/NavigationActivity.java
+++ app/src/main/java/com/infobeacons/NavigationActivity.java
@@ -5,27 +5,40 @@ import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class NavigationActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
+    private String TAG = "NavigationActivity";
+
+    private static final Region ALL_ESTIMOTE_BEACONS_REGION = new Region("rid", null, null, null);
     private static final String NAO_SUPORTA_TTS = "Seu dispositvo não suporta o texto para voz";
 
+    private BeaconManager mBeaconManager;
+
     private TextToSpeech mTts;
-    final String msg = "mensagem teste";
+    private String msg = "mensagem teste";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        final TextView mteTextView = (TextView) findViewById(R.id.texto);
+        final TextView mTextView = (TextView) findViewById(R.id.texto);
         mTts = new TextToSpeech(NavigationActivity.this, NavigationActivity.this);
         setSupportActionBar(toolbar);
 
@@ -36,6 +49,47 @@ public class NavigationActivity extends AppCompatActivity implements TextToSpeec
                 mTts.speak(msg, TextToSpeech.QUEUE_ADD, null);
             }
         });
+
+        mBeaconManager = new BeaconManager(getApplicationContext());
+        mBeaconManager.setForegroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+
+        mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                mBeaconManager.startMonitoring(ALL_ESTIMOTE_BEACONS_REGION);
+            }
+        });
+
+        mBeaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                Log.i(TAG, "procurando...");
+                if (!list.isEmpty()) {
+                    msg = list.get(0).getMacAddress().toStandardString();
+                    mTextView.setText(msg);
+                } else {
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                mBeaconManager.startRanging(ALL_ESTIMOTE_BEACONS_REGION);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        mBeaconManager.stopRanging(ALL_ESTIMOTE_BEACONS_REGION);
+        super.onPause();
     }
 
     @Override
@@ -61,5 +115,28 @@ public class NavigationActivity extends AppCompatActivity implements TextToSpeec
         } else {
             Toast.makeText(getApplicationContext(), NAO_SUPORTA_TTS, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onStop() {
+        mTts.stop();
+        mTts.shutdown();
+        super.onStop();
+        Log.i(TAG, "onStop");
+        mBeaconManager.disconnect();
+    }
+
+    @Override
+    public void onDestroy() {
+        mTts.stop();
+        mTts.shutdown();
+        super.onDestroy();
+        mBeaconManager.disconnect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 }
